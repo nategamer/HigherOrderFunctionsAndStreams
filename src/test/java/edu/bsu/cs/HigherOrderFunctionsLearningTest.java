c258f0f4ee0fa3837204f917dc7644899501dd42
Nate <nategamer1997@gmail.com>
2021-05-03 16:52:34 -0400

    Completed 1-8

--- src/test/java/edu/bsu/cs/HigherOrderFunctionsLearningTest.java
+++ src/test/java/edu/bsu/cs/HigherOrderFunctionsLearningTest.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.time.Instant;
 import java.time.Month;
+import java.time.ZoneId;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -46,7 +47,7 @@ public class HigherOrderFunctionsLearningTest {
     @Test
     public void testMap() {
         Stream<Integer> input = Stream.of(1, 2, 3);
-        List<Integer> actual = null; // TODO: Replace the `actual` with your solution, here and in all the following.
+        List<Integer> actual = input.map(m -> m+1).collect(Collectors.toList());
         List<Integer> expected = List.of(2, 3, 4);
         Assertions.assertIterableEquals(expected, actual);
     }
@@ -57,7 +58,7 @@ public class HigherOrderFunctionsLearningTest {
     @Test
     public void testFilter() {
         Stream<String> input = Stream.of("a1", "a2", "a3", "b1", "b2", "b3");
-        List<String> actual = null;
+        List<String> actual = input.filter(m -> m.startsWith("b")).collect(Collectors.toList());
         List<String> expected = List.of("b1", "b2", "b3");
         Assertions.assertIterableEquals(expected, actual);
     }
@@ -69,7 +70,8 @@ public class HigherOrderFunctionsLearningTest {
     @Test
     public void testDistinct() {
         Stream<String> input = Stream.of("a1", "a2", "a3", "b1", "b2", "b3");
-        List<String> actual = null;
+        List<String> actual = input.map(m -> m.replaceAll("[0-9]", "")).distinct().collect(Collectors.toList());
+        System.out.println(actual);
         List<String> expected = List.of("a", "b");
         Assertions.assertIterableEquals(expected, actual);
     }
@@ -80,7 +82,8 @@ public class HigherOrderFunctionsLearningTest {
     @Test
     public void testConcatEven() {
         Stream<String> input = Stream.of("a1", "a2", "a3", "b1", "b2", "b3");
-        String actual = null;
+        String actual = input.filter(m -> Integer.parseInt("" + m.charAt(m.length()-1)) % 2 == 0).reduce("",(partial, element) -> partial + element);
+        System.out.println(actual);
         String expected = "a2b2";
         Assertions.assertEquals(expected, actual);
     }
@@ -92,7 +95,7 @@ public class HigherOrderFunctionsLearningTest {
     public void testCountByDate() {
         Instant instant = Instant.parse("2021-03-01T00:00:00.00Z");
         Stream<Revision> input = getRevisions("soup04.json");
-        long actual = 0;
+        long actual = input.filter(r -> r.timestamp.isBefore(instant)).count();
         int expected = 3;
         Assertions.assertEquals(expected, actual);
     }
@@ -127,7 +130,7 @@ public class HigherOrderFunctionsLearningTest {
     public void testCountWhitelisted() {
         List<String> whitelist = List.of("Sleepy Beauty", "Spencer", "QueasyQ");
         Stream<Revision> input = getRevisions("soup30.json");
-        long actual = 0;
+        long actual = input.filter(r -> whitelist.contains(r.user)).count();
         int expected = 3;
         Assertions.assertEquals(expected, actual);
     }
@@ -138,7 +141,10 @@ public class HigherOrderFunctionsLearningTest {
     @Test
     public void testCountNonBots() {
         Stream<Revision> input = getRevisions("soup30.json");
-        long actual = 0;
+        long actual = input.filter(r -> {
+                System.out.println(r.user);
+                return !r.user.matches("(?i)(.*)bot(.*)"); //returns opposite of match, to get the ones that dont have bot in name
+        }).count();
         int expected = 26;
         Assertions.assertEquals(expected, actual);
     }
@@ -149,11 +155,10 @@ public class HigherOrderFunctionsLearningTest {
     @Test
     public void testCountChangesInFebruary() {
         Stream<Revision> input = getRevisions("soup30.json");
-        long actual = 0;
+        long actual = input.filter(r -> r.timestamp.atZone(ZoneId.systemDefault()).getMonth() == Month.FEBRUARY).count();
         int expected = 9;
         Assertions.assertEquals(expected, actual);
     }
-
     /**
      * Count the number of changes made in each month (in UTC).
      */
